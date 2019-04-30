b6519fe6d73420909fbbabebcc5cb7e464faf2ee
Jordi <j.calveres.garcia@gmail.com>
2019-04-30 17:51:07 +0200

    Format

--- src/KeyStrokeListener.kt
+++ src/KeyStrokeListener.kt
@@ -7,7 +7,6 @@ import com.googlecode.lanterna.input.KeyType
 import java.util.concurrent.atomic.AtomicBoolean
 
 class KeyStrokeListener(private val gui: FirstGUI) : WindowListener {
-
     override fun onInput(basePane: Window, keyStroke: KeyStroke, deliverEvent: AtomicBoolean) {
         if (keyStroke.getKeyType() === KeyType.Escape) {
             gui.close()
@@ -15,14 +14,14 @@ class KeyStrokeListener(private val gui: FirstGUI) : WindowListener {
     }
 
     override fun onUnhandledInput(basePane: Window?, keyStroke: KeyStroke?, hasBeenHandled: AtomicBoolean?) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
     }
 
     override fun onResized(window: Window?, oldSize: TerminalSize?, newSize: TerminalSize?) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
     }
 
     override fun onMoved(window: Window?, oldPosition: TerminalPosition?, newPosition: TerminalPosition?) {
-        // TODO Auto-generated method stub
+        // Auto-generated method stub
     }
 }
